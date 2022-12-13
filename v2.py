60b5ea1079230957802a424212b968c6b8a75dbf
fatiholgunoz <96265225+fatiholgunoz@users.noreply.github.com>
2022-12-13 12:40:37 +0300

    Create v2.py

---
+++ v2.py
@@ -0,0 +1,26 @@
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def cypher(input_text, shift_num, mode):
+  output = []
+  if mode == "decode":
+    shift_num = shift_num * -1
+  for letter in input_text:
+    position = alphabet.index(letter)
+    newposition = (position + shift_num)%26
+    new_char = alphabet[newposition]
+    output.append(new_char)
+    merged = "".join(output)
+  print(f"The {mode}d text is {merged}")
+
+cypher(text, shift, direction)
+
+    #Example:
+    #plain_text = "hello"
+    #shift = 5
+    #print output: "The encoded text is mjqqt"
